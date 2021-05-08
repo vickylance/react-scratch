59cccde5a6ab90852a87d34530f1df7cab0d54ea
Vignesh S <vignesh.s1@thoughtworks.com>
2021-05-08 23:02:24 +0530

    Vignesh: added web-vitals reporting

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Button } from "./index.style";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './vitals';
+import { Button } from './index.style';
+import './index.css';
 
 ReactDOM.render(
   <h1>
     Hello World <Button>React!</Button>
   </h1>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
+
+reportWebVitals();
